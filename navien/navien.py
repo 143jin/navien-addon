cc76807e36d076e3cd841a9d6c9cd3bd7a2e5bee
143jin <143jin@gmail.com>
2025-12-17 00:51:30 +0900

    Update navien.py

--- navien/navien.py
+++ navien/navien.py
@@ -192,10 +192,10 @@ class Wallpad:
 wallpad = Wallpad()
 
 packet_2_payload_percentage = {'00': '0', '01': '1', '02': '2', '03': '3'}
-packet_2_payload_modes = {'01': 'bypass', '03': 'heating', '04': 'auto', '05': 'air_clean'}
+packet_2_payload_modes = {'01': 'bypass', '03': 'ventilation', '04': 'auto', '05': 'air_clean'}
 
 ### 환풍기 ###
-optional_info = {'optimistic': 'false', 'speed_range_min': 1, 'speed_range_max': 3,'preset_modes': ['bypass', 'heating', 'auto', 'air_clean']
+optional_info = {'optimistic': 'false', 'speed_range_min': 1, 'speed_range_max': 3,'preset_modes': ['bypass', 'ventilation', 'auto', 'air_clean']
 }
 환풍기 = wallpad.add_device(device_name = '전열교환기', device_id = '32', device_subid = '01', device_class = 'fan', optional_info = optional_info)
 환풍기.register_status(message_flag = '01', attr_name = 'availability', topic_class ='availability_topic',      regex = r'()', process_func = lambda v: 'online')
